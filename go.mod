d0db5965d46ae47fdd6809bef8e7b69422ba97fa
Michael McGowan <michael.mcgowan@students.makeschool.com>
2020-02-12 15:20:34 -0800

    [ADDED] Translate - NOTE: Rune without flag to use default flag to access static/views

--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module makesite
 
 go 1.13
+
+require (
+	cloud.google.com/go v0.52.0
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	go.opencensus.io v0.22.3 // indirect
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/sys v0.0.0-20200212091648-12a6c2dcc1e4 // indirect
+	golang.org/x/text v0.3.2
+	google.golang.org/api v0.17.0
+	google.golang.org/genproto v0.0.0-20200212121238-0849286c0f0e // indirect
+	google.golang.org/grpc v1.27.1 // indirect
+)
--- makesite.go
+++ makesite.go
@@ -4,15 +4,26 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 	"text/template"
+
+	"context"
+
+	"cloud.google.com/go/translate"
+	"golang.org/x/text/language"
 )
+
+// func main() {
+// 	fmt.Println(translateText("es", "Translate this string from english"))
+// }
 
 func main() {
 
-	filePtr := flag.String("dir", ".", " Help text.")
+	filePtr := flag.String("dir", "txt", " Help text.")
 	flag.Parse()
 	txtFiles := traverseFiles(*filePtr)
 	fmt.Println(txtFiles)
@@ -21,10 +32,13 @@ func main() {
 		fmt.Println("file opened:", file)
 	}
 
+	http.Handle("/", http.FileServer(http.Dir("./static/views")))
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
 }
 
-func readFile(filename string) string {
-	fileContents, err := ioutil.ReadFile(filename)
+func readFile(filepath string) string {
+	fileContents, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		panic(err)
 	}
@@ -41,37 +55,66 @@ func traverseFiles(dir string) []string {
 
 	for _, file := range files {
 		if filepath.Ext(file.Name()) == ".txt" {
-			output = append(output, file.Name())
+			output = append(output, dir+"/"+file.Name())
+			fmt.Println(file.Name())
 		}
 	}
 	return output
 }
 
 func extractFileName(filename string) string {
-
-	var newFileName string
-	newFileName = strings.SplitAfter(filename, ".")[0]
 	fileExt := "html"
-	fmt.Println(newFileName + fileExt)
-	return "./" + newFileName + fileExt
+	fmt.Println(filename + fileExt)
+	outfile := strings.Split(filename, "/")
+	outfile = (strings.Split(outfile[len(outfile)-1], "."))
+	fmt.Println("here")
+	fmt.Println(outfile[0])
+	fmt.Println("here")
+	return "./static/views/" + outfile[0] + "." + fileExt
 }
 
 func textToTemplate(filename string) {
 	fileContents := readFile(filename)
+	translatedFileContents, err := translateText("es", fileContents)
 	pathOut := extractFileName(filename)
-	tpl, err := template.ParseFiles("template.tmpl")
+	tpl, err := template.ParseFiles("./static/templates/template.tmpl")
 	check(err)
 
 	type Content struct {
 		Contents string
 	}
 	content := Content{
-		Contents: string(fileContents),
+		Contents: string(translatedFileContents),
 	}
 	f, err := os.Create(pathOut)
 	check(err)
 	err = tpl.Execute(f, content)
 	check(err)
+}
+
+func translateText(targetLanguage, text string) (string, error) {
+	// text := "The Go Gopher is cute"
+	ctx := context.Background()
+
+	lang, err := language.Parse(targetLanguage)
+	if err != nil {
+		return "", fmt.Errorf("language.Parse: %v", err)
+	}
+
+	client, err := translate.NewClient(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer client.Close()
+
+	resp, err := client.Translate(ctx, []string{text}, lang, nil)
+	if err != nil {
+		return "", fmt.Errorf("Translate: %v", err)
+	}
+	if len(resp) == 0 {
+		return "", fmt.Errorf("Translate returned empty response to text: %s", text)
+	}
+	return resp[0].Text, nil
 }
 
 func check(e error) {
